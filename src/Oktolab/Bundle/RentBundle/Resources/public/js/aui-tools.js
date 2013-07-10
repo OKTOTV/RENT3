65a20733c6b97c26313ba8cc5812964235adb020
Michael Hirschler <meh@okto.tv>
2013-07-10 14:56:24 +0200

    added possibility to use aui-datepicker in forms

---
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/aui-tools.js
@@ -0,0 +1,5 @@
+AJS.$(document).ready(function() {
+    AJS.$('.aui-date-picker').each(function() {
+        AJS.$(this).datePicker({'overrideBrowserDefault': true, 'firstDay': -1, 'languageCode': 'de'});
+    });
+});
