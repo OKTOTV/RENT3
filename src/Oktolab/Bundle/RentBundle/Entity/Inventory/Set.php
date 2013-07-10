ade032a8219b44d4a6187defec49e4396857bfa3
Robin Schmidt <rs@okto.tv>
2013-07-10 13:07:15 +0200

    started Sets.

---
+++ src/Oktolab/Bundle/RentBundle/Entity/Inventory/Set.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Oktolab\Bundle\RentBundle\Entity\Inventory;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Set
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Set
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=500)
+     */
+    private $description;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="items", type="array")
+     */
+    private $items;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Set
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Set
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set items
+     *
+     * @param array $items
+     * @return Set
+     */
+    public function setItems($items)
+    {
+        $this->items = $items;
+    
+        return $this;
+    }
+
+    /**
+     * Get items
+     *
+     * @return array 
+     */
+    public function getItems()
+    {
+        return $this->items;
+    }
+}
