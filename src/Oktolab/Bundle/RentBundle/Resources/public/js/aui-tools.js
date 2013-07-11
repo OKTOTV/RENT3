c06e0e4dd9d061bee8f6f79a90971fd2fdc98db8
Michael Hirschler <meh@okto.tv>
2013-07-11 15:58:40 +0200

    added jquery.calendar as plugin

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/aui-tools.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/aui-tools.js
@@ -2,4 +2,6 @@ AJS.$(document).ready(function() {
     AJS.$('.aui-date-picker').each(function() {
         AJS.$(this).datePicker({'overrideBrowserDefault': true, 'firstDay': -1, 'languageCode': 'de'});
     });
+
+    AJS.$('#dashboard-calendar').cal();
 });
