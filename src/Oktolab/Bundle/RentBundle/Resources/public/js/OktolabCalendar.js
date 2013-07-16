a13ab87e047c57a715dd7876c13f377e13f1b82d
Michael Hirschler <meh@okto.tv>
2013-07-16 17:28:51 +0200

    refactored calendar creation

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendar.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendar.js
@@ -3,30 +3,34 @@ var Oktolab = {};
 Oktolab.Calendar = function Calendar(container) {
     'use strict';
 
-    var options = {
+    this.options = {
         'container': container,
         'startdate': new Date(),
         'eventSrcPath': 'gone.json',
         'configSrcPath': 'gone.json' // timeblocks and inventory
     };
 
+    this.data = {
+        container: AJS.$(this.options.container),
+    };
+
     this.render = function () {
-        var $container = AJS.$(options.container),
-            $inventory = AJS.$(options.container + '> .calendar-inventory'),
-            $wrapper = AJS.$(options.container + '> .calendar-wrapper'),
-            wrapperLength = $container.width() - $inventory.width(), // set height for wrapper
-            $date = options.startdate,
+        this.renderBasicCalendar();
+        this.renderInventory();
+
+        var wrapperLength = this.data.container.width() - this.data.inventory.width(), // set height for wrapper
+            $date = this.options.startdate,
             $blocks = '',
             i = 0;
 
-        $wrapper.height($inventory.outerHeight(true));
+        /*$wrapper.height($inventory.outerHeight(true));
 
         // draw calendar background
         for (i; i <= (wrapperLength - 300); i += 100) {
             $blocks = $blocks + this.getBlockForDate($date);
             $date.setDate($date.getDate() + 1);
         }
-        $wrapper.append($blocks);
+        $wrapper.append($blocks); */
 
         /*/ draw events (for and so on ...)
         var position = AJS.$('#test-element').offset();
@@ -49,10 +53,53 @@ Oktolab.Calendar = function Calendar(container) {
         return '<div class="calendar-date"><div class="calendar-headline"><span class="calendar-title">' + date.getDate() + '.' + (date.getMonth() + 1) + '</span><div class="calendar-timeblock">09-12</div><div class="calendar-timeblock">17-20</div></div></div>';
     };
 
-    this.renderInventory = function () {
-        var inventory = '<div class="calendar-inventory"></div>';
+    this.getInventory = function () {
+        var $json       = AJS.$.parseJSON('{"stuff":["item","item","item"],"some more stuff":["item bar","item foo","item baz"]}'),
+            $key        = null, // will be used to iterate itemgroups
+            $item       = null, // will be used to iterate items
+            $inventory  = '';
 
-        //console.log(typeof container);
-        container.append(inventory);
+        for ($key in $json) {
+            //console.log($json[$key].length);
+            $inventory = $inventory +
+                    '<div class="calendar-inventory-group">' +
+                        '<strong>' + $key + '</strong>' +
+                        '<ul>';
+
+            for ($item in $json[$key]) {
+                //console.log($json[$key][$item]);
+                $inventory = $inventory + '<li><a href="#">' + $json[$key][$item] + '</a></li>';
+            }
+
+
+            $inventory = $inventory + '</ul>' + '</div>';
+            /*<div class="calendar-inventory-group">
+            <strong>Stuff</strong>
+            <ul>
+                <li><a href="#">Item Foo</a></li>
+                <li><a href="#">Item Bar</a></li>
+                <li><a href="#">Item Baz</a></li>
+                <li><a href="#" id="test-element">Item Cool</a></li>
+                <li><a href="#" id="test-element2">Item Foo</a></li>
+            </ul>
+        </div>*/
+        }
+
+        console.log($inventory);
+
+        return $inventory;
     };
+
+    this.renderInventory = function () {
+        var $inventory = this.getInventory();
+        console.log($inventory);
+        this.data.inventory.append($inventory);
+    };
+
+    this.renderBasicCalendar = function () {
+        this.data.container.append('<div class="calendar-inventory"></div><div class="calendar-wrapper"></div>');
+
+        this.data.inventory = this.data.container.children('.calendar-inventory');
+        this.data.wrapper = this.data.container.children('.calendar-wrapper');
+    }
 };
--- src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendarTest.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendarTest.js
@@ -10,6 +10,8 @@ test('1.1 Renders calendar', function () {
 
 test('1.2 Renders inventory', function () {
     var calendar = new Oktolab.Calendar('#qunit-calendar');
+
+    calendar.renderBasicCalendar();
     calendar.renderInventory();
 
     equal(1, AJS.$('#qunit-calendar > .calendar-inventory').length, "Exactly one inventory is rendered");
