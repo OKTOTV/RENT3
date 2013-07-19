e8e9465ea867c6a6b79e3ac55cc36de1f6195885
Michael Hirschler <meh@okto.tv>
2013-07-19 15:09:18 +0200

    fixed typo in test descriptions

--- src/Oktolab/Bundle/RentBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Oktolab/Bundle/RentBundle/Tests/Controller/DefaultControllerTest.php
@@ -9,14 +9,14 @@ class DefaultControllerTest extends WebTestCase
     public function testDashboardPageRendersCorrectly()
     {
         $this->client->request('GET', '/');
-        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Returns Status-Code 200');
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response is successful');
         $this->assertRegExp('/Dashboard/', $this->client->getResponse()->getContent());
     }
 
     public function testAboutPageRendersCorrectly()
     {
         $this->client->request('GET', '/about');
-        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Returns Status-Code 200');
+        $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response is successful');
         $this->assertRegExp('/Lizenzen/', $this->client->getResponse()->getContent());
     }
 }
