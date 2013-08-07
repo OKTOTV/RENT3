0ed6cb2cb1bfb71f6a9389e45152d89cac84004f
Michael Hirschler <meh@okto.tv>
2013-08-07 08:59:32 +0200

    Refactored JS

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/aui-tools.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/aui-tools.js
@@ -1,15 +1,15 @@
 AJS.$(document).on('typeahead:initialized', function(e) {
-    // TODO: I wasn't able to do this in CSS, transfer it to CSS!
+    // typeahead CSS/AUI fix (Better solution: use raw CSS)
     AJS.$(e.target).siblings('.tt-hint').addClass('text'); // add aui-class "text" to hint-input
 });
 
 AJS.$(document).ready(function() {
+    // AUI Date-Picker activation
     AJS.$('.aui-date-picker').each(function() {
         AJS.$(this).datePicker({'overrideBrowserDefault': true, 'firstDay': -1, 'languageCode': 'de'});
     });
-});
 
-AJS.$(document).ready(function() {
+    // Activate Oktolab.Calendar on Dashboard
     if (AJS.$('#calendar').length) {
         var calendar = new Oktolab.Calendar('#calendar');
 
@@ -17,32 +17,4 @@ AJS.$(document).ready(function() {
         calendar.render();
         console.timeEnd('rendercalendar');
     }
-});
-
-
-var createRentForm = new AJS.Dialog({
-    id:                     'create-rent-form-dialog',
-    closeOnOutsideClick:    false,
-    width:                  700,
-    height:                 900,
-});
-
-createRentForm.addHeader('Neue Reservierung');
-createRentForm.addPanel('Inventar', '#rent-inventory-form');
-createRentForm.addPanel('R&auml;ume', '#rent-room-form');
-createRentForm.addButton('Reservieren', function (dialog, page) {
-    console.log(dialog);
-    console.log(page);
-    console.log(dialog.getCurrentPanel());
-
-    AJS.$('#rent-inventory-form > form').submit();
-});
-
-createRentForm.addLink('Abbrechen', function(dialog) {
-    dialog.hide();
-});
-
-AJS.$("#create-button").click(function() {
-    createRentForm.gotoPanel(0);
-    createRentForm.show();
 });
---
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/event/rentDialog.js
@@ -0,0 +1,30 @@
+/*
+ * Creates the Rent-Dialog in AppHeader
+ */
+
+var createRentForm = new AJS.Dialog({
+    id:                     'create-rent-form-dialog',
+    closeOnOutsideClick:    false,
+    width:                  800,
+    height:                 800,
+});
+
+createRentForm.addHeader('Neue Reservierung');
+createRentForm.addPanel('Inventar', '#rent-inventory-form');
+createRentForm.addPanel('R&auml;ume', '#rent-room-form');
+createRentForm.addButton('Reservieren', function (dialog, page) {
+    console.log(dialog);
+    console.log(page);
+    console.log(dialog.getCurrentPanel());
+
+    AJS.$('#rent-inventory-form > form').submit();
+});
+
+createRentForm.addLink('Abbrechen', function(dialog) {
+    dialog.hide();
+});
+
+AJS.$("#create-button").click(function() {
+    createRentForm.gotoPanel(0);
+    createRentForm.show();
+});
