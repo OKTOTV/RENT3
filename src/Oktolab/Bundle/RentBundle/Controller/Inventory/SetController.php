a3ff3bbd7b81364f3c5abd3b6b31428ddb471aef
Robin Schmidt <rs@okto.tv>
2013-08-08 10:12:02 +0200

    sets can now have a picture

--- src/Oktolab/Bundle/RentBundle/Controller/Inventory/SetController.php
+++ src/Oktolab/Bundle/RentBundle/Controller/Inventory/SetController.php
@@ -6,11 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Oktolab\Bundle\RentBundle\Entity\Inventory\Set;
 use Oktolab\Bundle\RentBundle\Form\Inventory\SetType;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Oktolab\Bundle\RentBundle\Entity\Inventory\Attachment;
+use Oktolab\Bundle\RentBundle\Form\Inventory\PictureType;
 /**
  * Inventory\Set controller.
  *
@@ -156,64 +158,38 @@ class SetController extends Controller
      * Finds and displays a Inventory\Set entity.
      *
      * @Route("/{id}", name="inventory_set_show")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
      * @Method("GET")
-     * @Template()
+     * @Template("OktolabRentBundle:Inventory\Set:show.html.twig", vars={"set"})
      */
-    public function showAction($id)
+    public function showAction(Set $set)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('OktolabRentBundle:Inventory\Set')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Inventory\Set entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),
-            'items'       => $entity->getItems(),
-        );
     }
 
     /**
      * Displays a form to edit an existing Inventory\Set entity.
      *
      * @Route("/{id}/edit", name="inventory_set_edit")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
      * @Method("GET")
-     * @Template()
+     * @Template
      */
-    public function editAction($id)
+    public function editAction(Set $set)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('OktolabRentBundle:Inventory\Set')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Inventory\Set entity.');
-        }
-
         $editForm = $this->createForm(
             new SetType(),
-            $entity,
+            $set,
             array(
                 'method' => 'PUT',
                 'action' => $this->generateUrl(
                     'inventory_set_update',
-                    array( 'id' => $id )
+                    array( 'id' => $set->getId() )
                 )
             )
         );
-
-        $deleteForm = $this->createDeleteForm($id);
-
         return array(
-            'entity'      => $entity,
+            'set'      => $set,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-            'items'       => $entity->getItems()
         );
     }
 
@@ -221,21 +197,13 @@ class SetController extends Controller
      * Edits an existing Inventory\Set entity.
      *
      * @Route("/{id}", name="inventory_set_update")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
      * @Method("PUT")
      * @Template("OktolabRentBundle:Inventory\Set:edit.html.twig")
      */
-    public function updateAction(Request $request, $id)
+    public function updateAction(Request $request, Set $set)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('OktolabRentBundle:Inventory\Set')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Inventory\Set entity.');
-        }
-        $items = $entity->getItems();
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new SetType(), $entity, array('method' => 'PUT'));
+        $editForm = $this->createForm(new SetType(), $set, array('method' => 'PUT'));
         $editForm->handleRequest($request);
 
         if ($editForm->isValid()) {
@@ -243,7 +211,7 @@ class SetController extends Controller
             $formItems = $editForm->get('itemsToAdd')->getData();
 
             //iterate the form items to update set
-            foreach ($entity->getItems() as $Item) {
+            foreach ($set->getItems() as $Item) {
 
                 if (!array_key_exists($Item->getId(), $formItems)) {
                     //Item is in Database but not in form, remove it
@@ -257,7 +225,7 @@ class SetController extends Controller
             foreach ($formItems as $key => $value) {
                 $Item = $em->getRepository('OktolabRentBundle:Inventory\Item')->find($key);
                 if ($Item) {
-                    $Item->setSet($entity);
+                    $Item->setSet($set);
                     $em->persist($Item);
 
                 } else {
@@ -270,19 +238,17 @@ class SetController extends Controller
             $files = $manager->uploadFiles();
 
             $uploader = $this->get('oktolab.upload_manager');
-            $uploader->saveAttachmentsToEntity($entity, $files);
+            $uploader->saveAttachmentsToEntity($set, $files);
             //-----------------------------
-            $em->persist($entity);
+            $em->persist($set);
             $em->flush();
 
-            return $this->redirect($this->generateUrl('inventory_set_show', array('id' => $id, 'items' => $items)));
+            return $this->redirect($this->generateUrl('inventory_set_show', array('id' => $set->getId())));
         }
 
         return array(
-            'entity'      => $entity,
+            'set'      => $set,
             'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-            'items'       => $items
         );
     }
 
@@ -290,27 +256,21 @@ class SetController extends Controller
      * Deletes a Inventory\Set entity.
      *
      * @Route("/delete/{id}", name="inventory_set_delete")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
      * @Method("GET")
      * @Template()
      */
-    public function deleteAction($id)
+    public function deleteAction(Set $set)
     {
-        $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository('OktolabRentBundle:Inventory\Set')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Inventory\Set entity.');
-        }
-
-        foreach ($entity->getItems() as $Item) {
+        foreach ($set->getItems() as $Item) {
             $Item->setSet();
         }
 
-        $em->remove($entity);
+        $em->remove($set);
 
         //TODO: create service --------
         $fileManager = $this->get('oktolab.upload_manager');
-        foreach ($entity->getAttachments() as $attachment) {
+        foreach ($set->getAttachments() as $attachment) {
             $fileManager->removeUpload($attachment);
             $em->remove($attachment);
         }
@@ -324,46 +284,92 @@ class SetController extends Controller
      * Remove an Item from a set entity.
      *
      * @Route("{setid}/remove/item/{id}", name="inventory_set_remove_item")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set", options={"id" = "setid"})
+     * @ParamConverter("item", class="OktolabRentBundle:Inventory\Item", options={"id" = "id"})
      * @Method("GET")
      */
-    public function removeItemAction($id, $setid)
+    public function removeItemAction(Item $item, Set $set)
     {
-        $em = $this->getDoctrine()->getManager();
+        $item->setSet();
+        $em->flush($item);
 
-        $item = $em->getRepository('OktolabRentBundle:Inventory\Item')->find($id);
-        $set = $em->getRepository('OktolabRentBundle:Inventory\Set')->find($setid);
-        if (!$item) {
-            $this->get('session')->getFlashBag()->add(
-                'error',
-                'Dieses Item konnte nicht gefunden werden.'
-            );
-        } else {
-            $item->setSet();
-            $em->flush($item);
-
-            //TODO: redirect back to Set edit.
-            $this->get('session')->getFlashBag()->add(
-                'notice',
-                sprintf('Item %s wurde erfolgreich aus Set entfernt.', $item->getTitle())
-            );
-        }
-        if (!$set) {
-            return $this->redirect($this->generateUrl('inventory_set'));
-        }
-        return $this->redirect($this->generateUrl('inventory_set_edit', array('id' => $setid)));
+        //TODO: redirect back to Set edit.
+        $this->get('session')->getFlashBag()->add(
+            'notice',
+            sprintf('Item %s wurde erfolgreich aus Set entfernt.', $item->getTitle())
+        );
+        return $this->redirect($this->generateUrl('inventory_set_edit', array('id' => $set->getId())));
     }
 
     /**
-     * Creates a form to delete a Inventory\Set entity by id.
+     * Deletes an attachment from the entity
      *
-     * @param mixed $id The entity id
-     *
-     * @return \Symfony\Component\Form\Form The form
+     * @Route("/{entity_id}/{attachment_id}/delete", name="inventory_set_attachment_delete")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set", options={"id" = "entity_id"})
+     * @ParamConverter("attachment", class="OktolabRentBundle:Inventory\Attachment", options={"id" = "attachment_id"})
+     * @Method("GET")
      */
-    private function createDeleteForm($id)
+    public function deleteAttachment(Set $set, Attachment $attachment)
     {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm();
+        $fileManager = $this->get('oktolab.upload_manager');
+        if ($attachment === $set->getPicture()) {
+            $set->setPicture();
+        } else {
+            $set->removeAttachment($attachment);
+        }
+
+        $fileManager->removeUpload($attachment);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($set);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('inventory_set_edit', array('id' => $set->getId())));
+    }
+
+        /**
+     * @Route("/{id}/picture/upload", name="inventory_set_picture_upload")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
+     * @Method("GET")
+     * @Template("OktolabRentBundle:Inventory\Set:edit_picture.html.twig")
+     */
+    public function uploadPictureAction(Set $set)
+    {
+        $picture = new Attachment();
+        $form   = $this->createForm(
+            new PictureType(),
+            $picture,
+            array(
+                'action' => $this->generateUrl('inventory_set_picture_update', array('id' => $set->getId())),
+                'method' => 'PUT'
+                )
+        );
+
+        return array(
+            'entity' => $set,
+            'edit_form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * @Route("/{id}/picture/upload", name="inventory_set_picture_update")
+     * @ParamConverter("set", class="OktolabRentBundle:Inventory\Set")
+     * @Method("PUT")
+     */
+    public function updatePictureAction(Set $set)
+    {
+        //TODO: move to service? -------
+        $manager = $this->get('oneup_uploader.orphanage_manager')->get('gallery');
+        $files = $manager->uploadFiles();
+
+        $uploader = $this->get('oktolab.upload_manager');
+        $uploader->saveAttachmentsToEntity($set, $files, true);
+        //-----------------------------
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($set);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('inventory_set_show', array('id' => $set->getId())));
     }
 }
