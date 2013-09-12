468b6fe77516b5616a7e4977ce9265910c9e45f8
Robin Schmidt <rs@okto.tv>
2013-09-12 14:16:55 +0200

    new translations and contextual menu in admin place interface.

--- src/Oktolab/Bundle/RentBundle/Form/Inventory/PlaceType.php
+++ src/Oktolab/Bundle/RentBundle/Form/Inventory/PlaceType.php
@@ -14,7 +14,7 @@ class PlaceType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title');
+        $builder->add('title', 'text', array('label' => 'inventory.place.title'));
     }
 
     /**
