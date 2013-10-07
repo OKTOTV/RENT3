9316b363059c7007fe80d95d4fd2764e4d2ea591
Michael Hirschler <michael.vhirsch@gmail.com>
2013-10-07 21:58:02 +0200

    Refactor: Appended DataFixtures to load with Rebuild-Script

---
+++ src/Oktolab/Bundle/RentBundle/DataFixtures/ORM/TimeblockFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Oktolab\Bundle\RentBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Oktolab\Bundle\RentBundle\Entity\Timeblock;
+
+/**
+ * Timeblock Fixtures
+ */
+class TimeblockFixture extends AbstractFixture
+{
+    /**
+     * Loads two Timeblocks from 08:00 - 12:00 and 13:00 - 17:00.
+     *
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $om)
+    {
+        $timeblockA = new Timeblock();
+        $timeblockA->setWeekdays(1016)   // all weekdays
+                ->setIntervalBegin(new \DateTime('2013-01-01 00:00'))
+                ->setIntervalEnd(new \DateTime('2020-12-31 23:59'))
+                ->setBegin(new \DateTime('2013-01-01 08:00'))
+                ->setEnd(new \DateTime('2013-01-01 12:00'))
+                ->setIsActive(true);
+
+        $timeblockB = clone $timeblockA;
+        $timeblockB->setBegin(new \DateTime('2013-01-01 13:00'))
+                ->setEnd(new \DateTime('2013-01-01 17:00'));
+
+        $om->persist($timeblockA);
+        $om->persist($timeblockB);
+
+        $om->flush();
+    }
+}
