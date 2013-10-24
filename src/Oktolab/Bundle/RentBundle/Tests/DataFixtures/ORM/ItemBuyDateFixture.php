7748d4f8bc361c9d5dee1bf26fdb1d656c0d94e8
Richard Treiblmaier <rt@okto.tv>
2013-10-24 11:46:02 +0200

    RENT-132:Fixed rendering of ItemBuyDate

---
+++ src/Oktolab/Bundle/RentBundle/Tests/DataFixtures/ORM/ItemBuyDateFixture.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Oktolab\Bundle\RentBundle\Tests\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Oktolab\Bundle\RentBundle\Entity\Inventory\Item;
+use Oktolab\Bundle\RentBundle\Entity\Inventory\Place;
+
+/**
+ *  Loads a fixture Item.
+ */
+class ItemBuyDateFixture extends AbstractFixture
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $place = new Place();
+        $place->setTitle('Test Place');
+
+        $itemA = new Item();
+        $itemA->setTitle('With Buy Date')
+                ->setDescription('An item with a buy date.')
+                ->setBarcode('123456')
+                ->setPlace($place)
+                ->setBuyDate(new \DateTime('1991-10-27 12:00:00'));
+
+        $itemB = new Item();
+        $itemB->setTitle('Without Buy Date')
+                ->setDescription('An item without a buy date.')
+                ->setBarcode('123456')
+                ->setPlace($place);
+
+        $manager->persist($place);
+        $manager->persist($itemA);
+        $manager->persist($itemB);
+
+        $manager->flush();
+    }
+}
