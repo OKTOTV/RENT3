88d4e3f3f9df7df96548e4da14967a791bc4c97f
Michael Hirschler <meh@okto.tv>
2013-10-28 15:56:09 +0100

    RENT-62: Fixed rendering of multiple inventory objects

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendar.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/OktolabCalendar.js
@@ -6,7 +6,7 @@
      *
      * @TODO: scrolling features
      * @TODO: reloading events automatically
-     * @TODO: use Hogan for template rendering
+     * @TODO: Build as jQuery plugin
      *
      * @type Object
      */
@@ -26,7 +26,7 @@
                 inventorySrcUri: '/api/calendar/inventory.json'
             };
 
-            $.extend(Calendar.config, settings);
+            $.extend({}, Calendar.config, settings);
 
             Calendar.data = {
                 container: $(Calendar.config.container),
@@ -136,54 +136,91 @@
          */
         showEvents: function (events) {
             $.each(events, function (key, event) {
-                var inventoryObject = Calendar._getInventoryObjectByIdentifier(event.objects[0].object_id.toLowerCase());
                 var begin = Calendar.findBlockByDate(new Date(event.begin));
                 var end = Calendar.findBlockByDate(new Date(event.end));
 
-                if (typeof(inventoryObject) === 'undefined') {
-                    return true;    // skip object
-                }
+                $.each(event.objects, function (key, object) {
+                    var inventoryObject = Calendar._getInventoryObjectByIdentifier(object.object_id.toLowerCase());
+                    if (typeof(inventoryObject) === 'undefined') {
+                        return true;    // skip object
+                    }
 
-                Calendar._renderEvent(event, inventoryObject, begin.block, end.block);
+                    var eventIdentifier = Calendar._renderEventChip(event, inventoryObject, begin.block, end.block);
+                    Calendar._renderEventDescription(event, eventIdentifier);
+                });
             });
         },
 
         /**
-         * Renders an Event.
+         * Renders an Event Chip (that small thing you see on Calendar).
          *
          * @param {object} event
+         * @param {object|jQuery} inventoryObject
+         * @param {object|jQuery} begin
+         * @param {object|jQuery} end
+         *
+         * @returns {String}
          */
-        _renderEvent: function (event, inventoryObject, begin, end) {
-            var template = '<div class="calendar-event" style="position:absolute; top:{{style_top}}px; left:{{style_left}}px; width:{{style_width}}px" id="event-{{id}}"><a href="#"><span class="aui-icon aui-icon-small aui-iconfont-info">Info</span></a><strong style="display: block; width: 100%; height: inherit">{{title}}</strong></div>';
+        _renderEventChip: function (event, inventoryObject, begin, end) {
+            var template = '<div class="calendar-event" style="position:absolute; top:{{style_top}}px; left:{{style_left}}px; width:{{style_width}}px" id="{{eventIdentfier}}"><a href="#"><span class="aui-icon aui-icon-small aui-iconfont-info">Info</span></a><strong style="display: block; width: 100%; height: inherit">{{title}}</strong></div>';
             var template = Hogan.compile(template);
-            var description = '<div class="calendar-event-description"><div class="event-image"><img width="50px" height="50px" src="{{image}}" alt="{{image_alt}}" /></div><div class="event-fields"><div class="event-field event-summary"><strong>{{name}}</strong> <span class="aui-lozenge">{{state}}</span></div><div class="event-field event-duration"><em>{{begin_view}}</em> - <em>{{end_view}}</em></div><div class="event-field event-description">{{description}}</div><div class="event-field event-objects">{{{objects}}}</div></div><div class="event-controls buttons-container"><div class="buttons"><a class="aui-button aui-button-link" href="{{uri}}">Loeschen</a> <span class="event-hyperlink-separator">·</span><a class="aui-button aui-button-primary" href="{{uri}}">Bearbeiten</a></div></div></div>';
-            var description = Hogan.compile(description);
-
-            console.log(begin, end);
-            Calendar.data.containerWrapper.append(template.render($.extend(event, {
+            var objectId = 'event-' + event.id + '-' + inventoryObject.attr('id').replace(':', '-');
+            var eventView = $.extend({}, event, {
                 style_top:      inventoryObject.position().top + 20,
                 style_left:     begin.offset().left,
                 style_width:    end.offset().left - begin.offset().left + end.width(),
-                image:          oktolab.baseUrl + '/../aui-5.1/images/user-avatar-blue-48@2x.png', //'http://placekitten.com/g/50/50',
-                objects:        Calendar._getRenderedEventObjects(event),
-            })));
+                eventIdentfier: objectId,
+            });
 
-            AJS.InlineDialog(AJS.$('#event-' + event.id), 1,
+            Calendar.data.containerWrapper.append(template.render(eventView));
+
+            return objectId;
+        },
+
+        /**
+         * Renders the Event description and triggers AJS.InlineDialog
+         *
+         * @param {object} event
+         * @param {string} eventIdentifier
+         */
+        _renderEventDescription: function (event, eventIdentifier) {
+            var description = '<div class="calendar-event-description"><div class="event-image"><img width="50px" height="50px" src="{{image}}" alt="{{image_alt}}" /></div><div class="event-fields"><div class="event-field event-summary"><strong>{{name}}</strong> <span class="aui-lozenge">{{state}}</span></div><div class="event-field event-duration"><em>{{begin_view}}</em> - <em>{{end_view}}</em></div><div class="event-field event-description">{{description}}</div><div class="event-field event-objects">{{{objects}}}</div></div><div class="event-controls buttons-container"><div class="buttons"><a class="aui-button aui-button-link" href="{{uri}}">Loeschen</a> <span class="event-hyperlink-separator">·</span><a class="aui-button aui-button-primary" href="{{uri}}">Bearbeiten</a></div></div></div>';
+            var description = Hogan.compile(description);
+            var eventView = $.extend({}, event, {
+                image:          oktolab.baseUrl + '/../aui-5.1/images/user-avatar-blue-48@2x.png',
+                objects:        Calendar._getRenderedEventObjects(event),
+            });
+
+            AJS.InlineDialog($('#' + eventIdentifier), 1,
                 function(content, trigger, showPopup) {
-                    content.html(description.render(event));
+                    content.html(description.render(eventView));
                     showPopup();
                     return false;
-                }, { 'onHover': true, 'showDelay': 200, 'onTop': true, 'width': 350 }
+                }, { 'onHover': false, 'showDelay': 200, 'onTop': true, 'width': 350 }
             );
         },
 
+        /**
+         * Returns the DOM Inventory object by given id
+         *
+         * @param {String} id
+         *
+         * @returns {Calendar.data.items}
+         */
         _getInventoryObjectByIdentifier: function (id) {
             return Calendar.data.items[id];
         },
 
+        /**
+         * Builds an ul-list with EventObjects for rendering.
+         *
+         * @param {object} event
+         * @returns {String}
+         */
         _getRenderedEventObjects: function (event) {
             var renderObjects = '<ul>';
             $.each(event.objects, function (key, object) {
+                console.log(object);
                 renderObjects = renderObjects + '<li><a href="' + object.uri + '">'  + object.title + '</a></li>';
             });
 
@@ -209,5 +246,6 @@
         }
     };
 
+    // Registers OktolabCalendar
     window.Oktolab.Calendar = Calendar;
 }(window, document, jQuery, Oktolab));
