1340346e0b8d43562862b2f437bb7754276e5219
Robin Schmidt <rs@okto.tv>
2013-11-21 10:12:49 +0100

    items can now be removed in new reservation form. RENT-159

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/event/rentInventory.js
+++ src/Oktolab/Bundle/RentBundle/Resources/public/js/event/rentInventory.js
@@ -7,6 +7,7 @@ var collectionHolder = AJS.$('.event-objects');
  * @param TypeaheadDatum datum
  */
 oktolab.addTypeaheadObjectToEventForm = function(collectionHolder, datum) {
+    console.log('add item');
     var index    = collectionHolder.data('index');
     var template = Hogan.compile(collectionHolder.data('prototype'));
     var output   = template.render(AJS.$.extend(datum, { index: index + 1 }));
@@ -20,12 +21,12 @@ oktolab.addTypeaheadObjectToEventForm = function(collectionHolder, datum) {
  *
  * @param jQueryEvent event
  */
-oktolab.removeEventObjectFromEventForm = function(event) {
-    var object = AJS.$(this).data('value');
-    var form   = collectionHolder.closest('form');
+oktolab.removeEventObjectFromEventForm = function(object) {
+//    var object = AJS.$(this).data('value');
+//    var form   = collectionHolder.closest('form');
 
-    AJS.$(event.target).closest('tr').remove();
-    form.find('div[data-object="' + object + '"]').remove();
+    AJS.$(object).closest('tr').remove();
+//    form.find('div[data-object="' + object + '"]').remove();
 };
 
 AJS.$(document).ready(function() {
@@ -65,7 +66,10 @@ AJS.$(document).ready(function() {
         jQuery(this).typeahead('setQuery', '');
     });
 
-    collectionHolder.on('click', '.remove-object', oktolab.removeEventObjectFromEventForm);
+    collectionHolder.on('click', 'a.remove', function (e) {
+        e.preventDefault();
+        oktolab.removeEventObjectFromEventForm(e.currentTarget);
+    });
 });
 
 Oktolab.EventForm.init({ container: '#rent-inventory-form > form', hideButtons: true });
