f88a5413d5177e196431cc83bbcf2f20afac70f8
Richard Treiblmaier <rt@okto.tv>
2013-11-28 12:04:56 +0100

    php sniffer. Refactored ImportContactsCommand and DateTimeParamConverter

--- src/Oktolab/Bundle/RentBundle/Command/ImportContactsCommand.php
+++ src/Oktolab/Bundle/RentBundle/Command/ImportContactsCommand.php
@@ -8,68 +8,70 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Oktolab\Bundle\RentBundle\Model\ContactProvider;
 
+/**
+ * Command to import contacts from Oktolab HUB.
+ */
 class ImportContactsCommand extends ContainerAwareCommand
 {
-     protected function configure()
-     {
-         $this->setName('rentBundle:importContacts')
-                ->addOption('dryrun', 'd', InputOption::VALUE_NONE, 'Makes a Dryrun and gives you a number of contacts that WOULD be imported')
-                ->addArgument('name', InputOption::VALUE_REQUIRED, 'The string to search for in contactsapi');
-     }
+    /**
+     * {@inheritDoc}
+     */
+    protected function configure()
+    {
+        $this->setName('rent:import-contacts')
+               ->setDescription('Imports contacts from Oktolab HUB.')
+               ->addOption('dry-run', 'd', InputOption::VALUE_NONE, 'Makes a dry run.')
+               ->addArgument('name', InputOption::VALUE_REQUIRED, 'The name to search for in contacts API.');
+    }
 
-     protected function execute(InputInterface $input, OutputInterface $output)
-     {
-         $contactProvider = $this->getContainer()->get('oktolab.contact_provider');
+    /**
+     * {@inheritDoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $contactProvider = $this->getContainer()->get('oktolab.contact_provider');
+        $hubContacts = $contactProvider->getContactsByName($input->getArgument('name'));
+        $localContacts = $contactProvider->getContactsByName('*', ContactProvider::$Resource_RENT);
+        $newContacts = $this->getNewContacts($hubContacts, $localContacts);
 
-         if ($input->getOption('verbose')) {
-             $output->writeln('Start getting Contacts from FLOW');
-         }
+        if ($input->getOption('verbose')) {
+            $output->writeln(sprintf('Found %d contacts in Oktolab HUB.', count($hubContacts)));
+            $output->writeln(sprintf('Found %d contacts in Oktolab RENT.', count($localContacts)));
+            $output->writeln(sprintf('Will import %d contacts to Oktolab RENT.', count($newContacts)));
+        }
 
-         $hubContacts = $contactProvider->getContactsByName($input->getArgument('name'));
-
-         if ($input->getOption('verbose')) {
-             $output->writeln(sprintf('Found %s Contacts in FLOW', count($hubContacts)));
-         }
-
-         $localContacts = $contactProvider->getContactsByName('*', ContactProvider::$Resource_RENT);
-
-         if ($input->getOption('verbose')) {
-             $output->writeln(sprintf('Found %s Contacts in RENT (local)', count($localContacts)));
-             $output->writeln('Start search for new ones');
-         }
-
-         $newContacts = $this->getNewContacts($hubContacts, $localContacts);
-
-         if ($input->getOption('verbose')) {
-             $output->writeln(sprintf('Found %s Contacts to import to RENT!', count($newContacts)));
-         }
-
-         if (!$input->getOption('dryrun')) {
-            $output->writeln('Start import to Database. This could take a while');
+        if (!$input->getOption('dry-run')) {
+            $output->writeln('Importing data. This could take a while ...');
             $contactProvider->addContactsToRent($newContacts);
+
+            // @TODO: Check if the import was *really* successful (e.g. return true/false ...)
             $output->writeln('Import successful!');
-         }
+        }
+    }
 
-         if ($input->getOption('verbose') && !$input->getOption('dryrun')) {
-             $output->writeln(sprintf('%s Contacts now in RENT', count($contactProvider->getContactsByName('*', ContactProvider::$Resource_RENT))));
-         }
-     }
+    /**
+     * Returns delta of new Contacts.
+     *
+     * @param array $hubContacts
+     * @param array $localContacts
+     *
+     * @return array
+     */
+    private function getNewContacts($hubContacts, $localContacts)
+    {
+        if (!$localContacts) {
+            return $hubContacts;
+        }
 
-     private function getNewContacts($hubContacts, $localContacts)
-     {
-         $contacts = $hubContacts;
+        $contacts = $hubContacts;
+        foreach ($hubContacts as $key => $hubContact) {
+            foreach ($localContacts as $localContact) {
+                if ($hubContact->getGuid() == $localContact->getGuid()) {
+                    unset($contacts[$key]);
+                }
+            }
+        }
 
-         if ($localContacts) {
-             foreach($hubContacts as $key=>$hubContact) {
-
-                 foreach($localContacts as $localCostUnit) {
-
-                     if ($hubContact->getGuid() == $localCostUnit->getGuid()) {
-                         unset($contacts[$key]);
-                     }
-                 }
-             }
-         }
-         return $contacts;
-     }
+        return $contacts;
+    }
 }
