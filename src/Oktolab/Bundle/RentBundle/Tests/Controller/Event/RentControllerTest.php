3543629a8cdbb0911043b3626c1509c762c7bb80
Richard Treiblmaier <rt@okto.tv>
2014-02-27 13:34:17 +0100

    Fixed test bugs.

--- src/Oktolab/Bundle/RentBundle/Tests/Controller/Event/RentControllerTest.php
+++ src/Oktolab/Bundle/RentBundle/Tests/Controller/Event/RentControllerTest.php
@@ -30,6 +30,11 @@ class RentControllerTest extends WebTestCase
      */
     public function rentInventoryRendersAsFragment()
     {
+        $this->loadFixtures(
+            array(
+                'Oktolab\Bundle\RentBundle\Tests\DataFixtures\ORM\Event\EventTypeFixture'
+            )
+        );
         $crawler = $this->client->request('GET', '/rent/inventory');
         $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response should be successful.');
         $this->assertSame(0, $crawler->filterXPath('body')->count(), 'No body should be rendered.');
@@ -45,6 +50,11 @@ class RentControllerTest extends WebTestCase
 
     public function testRenderRentRoomFormRendersCorrectly()
     {
+        $this->loadFixtures(
+            array(
+                'Oktolab\Bundle\RentBundle\Tests\DataFixtures\ORM\Event\EventTypeFixture'
+            )
+        );
         $this->client->request('GET', '/rent/room');
         $this->assertTrue($this->client->getResponse()->isSuccessful(), 'Response should be successful');
         $this->assertTrue($this->client->getResponse()->isCacheable(), 'Response should be cacheable');
