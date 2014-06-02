98e2c68706f7d7b49de76bee47c35943421bd057
Robin <rs@okto.tv>
2014-06-02 14:11:11 +0200

    removed file (not needed anymore)

--- src/Oktolab/Bundle/RentBundle/Resources/public/js/event/base-event.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function ($) {
-
-    // disable enter in input fields to prevent form submit
-    //$('input,select').keypress(function(event) { return event.keyCode != 13; });
-
-});
